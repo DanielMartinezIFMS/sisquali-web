e9e6954f199f5d98f86ccfee9c1b96c7831f0e81
Victor <victor.gonzalez@estudante.ifms.edu.br>
2021-08-26 14:21:30 -0400

    Implementação do cadastro de laboratório

--- src/components/parametros/LaboratorioCad.vue
+++ src/components/parametros/LaboratorioCad.vue
@@ -1,11 +1,39 @@
 <template>
-    <span> Implemente o cadastro de laboratorios aqui</span>
+    <cr-panel columns="1">
+                <cr-panel columns="2" boxShadow labelTop>
+                    <input-longtext label="Nome*" v-model="cadastro.nome"/>
+                    <input-combo label="Unidade*" :collection="unidades"/>
+                    <cr-panel columns="1">
+                        <input-integer label="Prazo de confirmação para contratação de cliente externo (dias)" v-model="cadastro.PrzContrCle"/>
+                        <input-integer label="Tempo de retenção de amostras para clientes internos (dias)" v-model="cadastro.PrzRetCli"/>
+                        <input-integer label="Tempo de retenção de amostras para clientes externos (dias)" v-model="cadastro.PrzRetCle"/>
+                    </cr-panel>
+                </cr-panel>
+    </cr-panel>
 </template>
 
 <script>
-    export default {
-        name: "LaboratorioCad"
-    }
+//import ctt from './Constants';
+import inputLongtext from "../framework/form/inputLongtext";
+import inputCombo from "../framework/form/inputCombo";
+import inputInteger from '../framework/form/inputInteger';
+import CrPanel from '../framework/form/crPanel';
+
+
+export default {
+    name: "LaboratorioCad",
+    components: {inputLongtext, inputCombo, CrPanel, inputInteger},
+
+    data: function () {
+        return {
+            unidades: [],
+            cadastro: {},
+        };
+    },
+    //async beforeCreate() {
+    //this.unidades=await this.$get(ctt.rest+'/unidade/listar');
+    //}
+}
 </script>
 
 <style scoped>
